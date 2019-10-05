72726d53537dfa0eccaca1e91e7b44c746138f63
João Vitor <joaooviitorr@hotmail.com>
2019-10-05 20:04:50 -0300

    SISMED

---
+++ src/main/java/br/com/sismed/DemoSismedApplication.java
@@ -0,0 +1,13 @@
+package br.com.sismed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSismedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSismedApplication.class, args);
+	}
+
+}
