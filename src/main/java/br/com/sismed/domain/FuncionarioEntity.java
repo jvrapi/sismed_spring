e33a02107802ed57f3ad14c1bc518e2954358657
Nadia Araujo <nadiaaraujo1@egmail.com>
2019-10-09 14:36:05 -0300

    Inclusao de funcionario

---
+++ src/main/java/br/com/sismed/domain/FuncionarioEntity.java
@@ -0,0 +1,167 @@
+package br.com.sismed.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class FuncionarioEntity {
+	
+	private String nome;
+	private String cpf;
+	private String rg;
+	private String orgao_emissor;
+	private String crm;
+	private String especialidade;
+	private String telefone;
+	private String celular;
+	private String sexo;
+	private String data_nascimento;
+	private String email;
+	private String estado_civil;
+	private String escolaridade;
+	private String formacao;
+	private String cep;
+	private String endereco;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String naturalidade;
+	private String nacionalidade;
+	private Integer nivel_acesso;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getOrgao_emissor() {
+		return orgao_emissor;
+	}
+	public void setOrgao_emissor(String orgao_emissor) {
+		this.orgao_emissor = orgao_emissor;
+	}
+	public String getCrm() {
+		return crm;
+	}
+	public void setCrm(String crm) {
+		this.crm = crm;
+	}
+	public String getEspecialidade() {
+		return especialidade;
+	}
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getData_nascimento() {
+		return data_nascimento;
+	}
+	public void setData_nascimento(String data_nascimento) {
+		this.data_nascimento = data_nascimento;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEstado_civil() {
+		return estado_civil;
+	}
+	public void setEstado_civil(String estado_civil) {
+		this.estado_civil = estado_civil;
+	}
+	public String getEscolaridade() {
+		return escolaridade;
+	}
+	public void setEscolaridade(String escolaridade) {
+		this.escolaridade = escolaridade;
+	}
+	public String getFormacao() {
+		return formacao;
+	}
+	public void setFormacao(String formacao) {
+		this.formacao = formacao;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getNaturalidade() {
+		return naturalidade;
+	}
+	public void setNaturalidade(String naturalidade) {
+		this.naturalidade = naturalidade;
+	}
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+	public Integer getNivel_acesso() {
+		return nivel_acesso;
+	}
+	public void setNivel_acesso(Integer nivel_acesso) {
+		this.nivel_acesso = nivel_acesso;
+	}
+	
+	
+	
+
+
+}
