2cfa7718cd0f4330c6eeb501459269bc701ffa5f
leonardorochaf <leonardo.rocha0803@gmail.com>
2019-10-10 11:29:11 -0300

    correções service paciente e registro clinico

--- src/main/java/br/com/sismed/service/PacienteServiceImpl.java
+++ src/main/java/br/com/sismed/service/PacienteServiceImpl.java
@@ -3,11 +3,13 @@ package br.com.sismed.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.sismed.dao.PacienteDao;
 import br.com.sismed.domain.Paciente;
 
+@Service @Transactional
 public class PacienteServiceImpl implements PacienteService{
 
 	@Autowired
--- src/main/java/br/com/sismed/service/RegistroClinicoServiceImpl.java
+++ src/main/java/br/com/sismed/service/RegistroClinicoServiceImpl.java
@@ -3,11 +3,13 @@ package br.com.sismed.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.sismed.dao.RegistroClinicoDao;
 import br.com.sismed.domain.RegistroClinico;
 
+@Service @Transactional
 public class RegistroClinicoServiceImpl implements RegistroClinicoService{
 
 	@Autowired
