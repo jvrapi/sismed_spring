9ab36b4fef4a49c4f7fbd10bb32529f4aa4c8b85
leonardorochaf <leonardo.rocha0803@gmail.com>
2019-10-10 11:13:52 -0300

    Criação dao paciente e registro clinico; Criação service paciente e registro clinico

---
+++ src/main/java/br/com/sismed/service/RegistroClinicoServiceImpl.java
@@ -0,0 +1,44 @@
+package br.com.sismed.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.sismed.dao.RegistroClinicoDao;
+import br.com.sismed.domain.RegistroClinico;
+
+public class RegistroClinicoServiceImpl implements RegistroClinicoService{
+
+	@Autowired
+	private RegistroClinicoDao dao;
+	
+	@Override
+	public void salvar(RegistroClinico registro_clinico) {
+		dao.save(registro_clinico);
+	}
+
+	@Override
+	public void editar(RegistroClinico registro_clinico) {
+		dao.update(registro_clinico);
+	}
+
+	@Override
+	public void excluir(Long id) {
+		dao.delete(id);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public RegistroClinico buscarporId(Long id) {
+		
+		return dao.findById(id);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<RegistroClinico> buscarTodos() {
+		
+		return dao.findAll();
+	}
+}
