97ef933a1e4ab0b63655bc085a08104d64a50f89
João Vitor <joaooviitorr@hotmail.com>
2019-11-05 22:37:24 -0300

    Correções

--- src/main/resources/static/js/convenio/editar.js
+++ src/main/resources/static/js/convenio/editar.js
@@ -1,9 +1,10 @@
 
+	
 			function executar(botao) {
 				$(botao).remove();
 				$(".classid").remove();
 				$("#div_botao").append("<button class='btn btn-primary float-right' id='btn_submit' type='submit'>Salvar</button>");
-				$("#div_botao").append("<button class='btn btn-secondary mr-2 float-right' id='btn_cancelar' type='button' onclick='voltar(this)'>Cancelar</button>");
+				$("#div_botao").append("<button class='btn btn-secondary mr-2 float-right' id='btn_cancelar' style='margin-left: 10px;' type='button' onclick='voltar(this)'>Cancelar</button>");
 				$("input").prop("disabled", false);
 				$("select").prop("disabled", false);
 				$('#form').find(':input').each(function(i, elem) {
@@ -15,11 +16,12 @@
 				$(botao).remove();
 				$("#btn_submit").remove();
 				$("#div_botao").append("<button type='button' class='btn btn-danger classid float-right' data-toggle='modal' data-target='#myModal'>Excluir</button>");
-				$("#div_botao").append("<button class='btn btn-primary mr-2 float-right' id='btn_editar' type='button' onclick='executar(this)'>Editar</button>");
+				$("#div_botao").append("<button class='btn btn-primary mr-2 float-right' id='btn_editar' style='margin-left: 10px;' type='button' onclick='executar(this)'>Editar</button>");
 				$("input").prop("disabled", true);
 				$("select").prop("disabled", true);
 				$('#form').find(':input').each(function(i, elem) {
 			        $(this).val($(this).data("previous-value"));
 			    });
 			}
+		
 		
