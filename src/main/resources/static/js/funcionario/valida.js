8632e5e1383749262d4bc6d993c91465a476a21b
Nadia Araujo <nadiaaraujo1@egmail.com>
2019-12-13 08:59:34 -0300

    Funcionario

--- src/main/resources/static/js/funcionario/valida.js
+++ src/main/resources/static/js/funcionario/valida.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 		$("#tipo").change(function() {
 			var id =  $(this).val();
 			if(id == 2 ){
-				alert("Opção escolhida: Funcionário");
+			
 				$("#especialidade").prop("disabled", true);
 				$("#especialidade").val("");
 				$("#crm").prop("disabled", true);
 				$("#crm").val("");
 				
 			}else{
-				alert("Opção escolhida: Médico");
+				
 				$("#especialidade").prop("disabled", false);
 				$("#especialidade").val("");
 				$("#crm").prop("disabled", false);
