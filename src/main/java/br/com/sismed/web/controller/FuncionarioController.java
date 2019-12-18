7a414b007326deade0b1aa5cd4f4aa70d3e93584
Nadia Araujo <nadiaaraujo1@egmail.com>
2019-12-18 11:11:39 -0300

    Funcionario

--- src/main/java/br/com/sismed/web/controller/FuncionarioController.java
+++ src/main/java/br/com/sismed/web/controller/FuncionarioController.java
@@ -17,11 +17,13 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -37,6 +39,7 @@ import br.com.sismed.service.FuncTConvService;
 import br.com.sismed.service.FuncionarioService;
 import br.com.sismed.service.LoginService;
 import br.com.sismed.service.TConvenioService;
+import groovyjarjarpicocli.CommandLine.Model;
 
 @Controller
 @RequestMapping("/funcionario")
@@ -130,6 +133,12 @@ public class FuncionarioController {
 		}
 		
 		return "redirect:/funcionario/listar";
+	}
+
+	@RequestMapping(value="/funcionario", method=RequestMethod.POST)
+	public String contactSubmit(@ModelAttribute Funcionario person, BindingResult bindingResult, Model model) {
+	   
+	    return "result";
 	}
 	
 	@GetMapping("/editar/{id}") 
