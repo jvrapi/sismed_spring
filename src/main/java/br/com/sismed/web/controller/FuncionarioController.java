b0be99c36e2951fc857e223f33242858b06ddef8
joaooviitorr <joaooviitorr@hotmail.com>
2019-12-30 12:33:14 -0300

    Log

--- src/main/java/br/com/sismed/web/controller/FuncionarioController.java
+++ src/main/java/br/com/sismed/web/controller/FuncionarioController.java
@@ -1,5 +1,8 @@
 package br.com.sismed.web.controller;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,12 +34,14 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.sismed.domain.FuncTConv;
 import br.com.sismed.domain.Funcionario;
 import br.com.sismed.domain.LabelValue;
+import br.com.sismed.domain.Log;
 import br.com.sismed.domain.Login;
 import br.com.sismed.domain.Perfil;
 import br.com.sismed.domain.TConvenio;
 import br.com.sismed.service.ConvenioService;
 import br.com.sismed.service.FuncTConvService;
 import br.com.sismed.service.FuncionarioService;
+import br.com.sismed.service.LogService;
 import br.com.sismed.service.LoginService;
 import br.com.sismed.service.TConvenioService;
 import groovyjarjarpicocli.CommandLine.Model;
@@ -44,126 +49,126 @@ import groovyjarjarpicocli.CommandLine.Model;
 @Controller
 @RequestMapping("/funcionario")
 public class FuncionarioController {
-	
+
 	@Autowired
 	private FuncionarioService service;
-	
+
 	@Autowired
 	private ConvenioService cService;
-	
+
 	@Autowired
 	private LoginService lservice;
-	
+
 	@Autowired
 	private TConvenioService tcService;
-	
+
 	@Autowired
 	private FuncTConvService ftcService;
-	
+
+	@Autowired
+	private LogService logservice;
+
 	@GetMapping("/listar")
-	public String listar(ModelMap model, @RequestParam(value = "page", required=false, defaultValue="1") int page){
-		PageRequest pagerequest = PageRequest.of(page-1,2,Sort.by("id").ascending());
+	public String listar(ModelMap model, @RequestParam(value = "page", required = false, defaultValue = "1") int page) {
+		PageRequest pagerequest = PageRequest.of(page - 1, 2, Sort.by("id").ascending());
 		Page<Funcionario> funcionario = service.buscarTodos(pagerequest);
 		model.addAttribute("funcionario", funcionario);
 		int lastPage = funcionario.getTotalPages();
-		
+
 		if (lastPage == 1) {
 			List<Integer> pageNumbers = IntStream.rangeClosed(1, 1).boxed().collect(Collectors.toList());
 			model.addAttribute("pageNumbers", pageNumbers);
-		}
-		else if(lastPage == 2) {
+		} else if (lastPage == 2) {
 			List<Integer> pageNumbers = IntStream.rangeClosed(1, lastPage).boxed().collect(Collectors.toList());
 			model.addAttribute("pageNumbers", pageNumbers);
-		}
-		else if (page == 2 && lastPage == 3) {
+		} else if (page == 2 && lastPage == 3) {
 			List<Integer> pageNumbers = IntStream.rangeClosed(1, page + 1).boxed().collect(Collectors.toList());
 			model.addAttribute("pageNumbers", pageNumbers);
-		}
-		else if(page == 1 || page == 2) {
+		} else if (page == 1 || page == 2) {
 			List<Integer> pageNumbers = IntStream.rangeClosed(1, page + 2).boxed().collect(Collectors.toList());
 			model.addAttribute("pageNumbers", pageNumbers);
-		}
-		else if(page > 2 && page < lastPage - 1) {
+		} else if (page > 2 && page < lastPage - 1) {
 			List<Integer> pageNumbers = IntStream.rangeClosed(page - 2, page + 2).boxed().collect(Collectors.toList());
 			model.addAttribute("pageNumbers", pageNumbers);
-		}
-		else if(page == lastPage - 1) {
-			List<Integer> pageNumbers = IntStream.rangeClosed(page-2, lastPage).boxed().collect(Collectors.toList());
+		} else if (page == lastPage - 1) {
+			List<Integer> pageNumbers = IntStream.rangeClosed(page - 2, lastPage).boxed().collect(Collectors.toList());
+			model.addAttribute("pageNumbers", pageNumbers);
+		} else if (page == lastPage) {
+			List<Integer> pageNumbers = IntStream.rangeClosed(lastPage - 2, lastPage).boxed()
+					.collect(Collectors.toList());
 			model.addAttribute("pageNumbers", pageNumbers);
 		}
-		else if(page == lastPage) {
-			List<Integer> pageNumbers = IntStream.rangeClosed(lastPage - 2, lastPage).boxed().collect(Collectors.toList());
-			model.addAttribute("pageNumbers", pageNumbers);
-		}
-		
-		return "/funcionario/lista"; 
+
+		return "/funcionario/lista";
 	}
-	
-	@GetMapping("/cadastrar") 
+
+	@GetMapping("/cadastrar")
 	public String cadastrar(Funcionario funcionario) {
 		return "/funcionario/cadastro";
 	}
-	
+
 	@PostMapping("/salvar")
-	public String salvar( Funcionario funcionario, RedirectAttributes attr, @RequestParam("password")String senha) {
+	public String salvar(Funcionario funcionario, RedirectAttributes attr, @RequestParam("password") String senha) {
 		try {
-		service.salvar(funcionario);
-		Login login = new Login();
-		String cpf = funcionario.getCpf();
-		login.setFuncionario_id(funcionario);
-		login.setCpf(cpf);
-		login.setSenha(new BCryptPasswordEncoder().encode(senha));
-		
-		Integer crm = funcionario.getCrm();
-		Perfil perfil = new Perfil();
-		
-		if(crm == null) {
-			
-			perfil.setId(2L);
-			login.setPerfis(perfil);
-		}else {
-			perfil.setId(1L);
-			login.setPerfis(perfil);
+			service.salvar(funcionario);
+			Login login = new Login();
+			String cpf = funcionario.getCpf();
+			login.setFuncionario_id(funcionario);
+			login.setCpf(cpf);
+			login.setSenha(new BCryptPasswordEncoder().encode(senha));
+
+			Integer crm = funcionario.getCrm();
+			Perfil perfil = new Perfil();
+
+			if (crm == null) {
+
+				perfil.setId(2L);
+				login.setPerfis(perfil);
+			} else {
+				perfil.setId(1L);
+				login.setPerfis(perfil);
+			}
+
+			lservice.salvar(login);
+			attr.addFlashAttribute("success", "Funcionário(a) cadastrado(a) com sucesso");
+		} catch (DataIntegrityViolationException ex) {
+			attr.addFlashAttribute("fail", "Cadastro não realizado, CPF já existente");
 		}
-		
-		lservice.salvar(login);
-		attr.addFlashAttribute("success","Funcionário(a) cadastrado(a) com sucesso");
-		}catch(DataIntegrityViolationException ex){
-			attr.addFlashAttribute("fail","Cadastro não realizado, CPF já existente");
-		}
-		
+
 		return "redirect:/funcionario/listar";
 	}
 
-	@RequestMapping(value="/funcionario", method=RequestMethod.POST)
+	@RequestMapping(value = "/funcionario", method = RequestMethod.POST)
 	public String contactSubmit(@ModelAttribute Funcionario person, BindingResult bindingResult, Model model) {
-	   
-	    return "result";
+
+		return "result";
 	}
-	
-	@GetMapping("/editar/{id}") 
-	public String preEditar(@PathVariable("id") Long id, ModelMap model, @ModelAttribute("FuncTConv") FuncTConv functconv) {
+
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model,
+			@ModelAttribute("FuncTConv") FuncTConv functconv) {
 		model.addAttribute("funcionario", service.buscarporId(id));
 		model.addAttribute("convenios", cService.BuscarConvFunc(id));
 		model.addAttribute("allconvenios", cService.findAll());
 		return "/funcionario/editar";
 	}
-	
+
 	@GetMapping("/buscar/{id}")
 	@ResponseBody
-	public List<LabelValue> buscar (@PathVariable("id")Integer id, @RequestParam (value="term", required=false, defaultValue="") String term){
+	public List<LabelValue> buscar(@PathVariable("id") Integer id,
+			@RequestParam(value = "term", required = false, defaultValue = "") String term) {
 		List<LabelValue> suggeestions = new ArrayList<LabelValue>();
-		
-		if(id == 1) {
+
+		if (id == 1) {
 			Funcionario allFuncionario = service.ListarFuncionarioId(term);
-			
-				LabelValue lv = new LabelValue();
-				lv.setLabel(allFuncionario.getNome());
-				lv.setValue(allFuncionario.getId());
-				suggeestions.add(lv);
-			}
-		
-		else if(id == 2) {
+
+			LabelValue lv = new LabelValue();
+			lv.setLabel(allFuncionario.getNome());
+			lv.setValue(allFuncionario.getId());
+			suggeestions.add(lv);
+		}
+
+		else if (id == 2) {
 			List<Funcionario> allFuncionario = service.ListarFuncionarioNome(term);
 			for (Funcionario funcionario : allFuncionario) {
 				LabelValue lv = new LabelValue();
@@ -172,8 +177,8 @@ public class FuncionarioController {
 				suggeestions.add(lv);
 			}
 		}
-		
-		else if(id == 3) {
+
+		else if (id == 3) {
 			List<Funcionario> allFuncionario = service.ListarFuncionarioCPF(term);
 			for (Funcionario funcionario : allFuncionario) {
 				LabelValue lv = new LabelValue();
@@ -182,8 +187,8 @@ public class FuncionarioController {
 				suggeestions.add(lv);
 			}
 		}
-	
-		else if(id == 4) {
+
+		else if (id == 4) {
 			List<Funcionario> allFuncionario = service.ListarFuncionarioCelular(term);
 			for (Funcionario funcionario : allFuncionario) {
 				LabelValue lv = new LabelValue();
@@ -192,8 +197,8 @@ public class FuncionarioController {
 				suggeestions.add(lv);
 			}
 		}
-		
-		else if(id == 5) {
+
+		else if (id == 5) {
 			List<Funcionario> allFuncionario = service.ListarFuncionarioCRM(term);
 			for (Funcionario funcionario : allFuncionario) {
 				LabelValue lv = new LabelValue();
@@ -202,8 +207,8 @@ public class FuncionarioController {
 				suggeestions.add(lv);
 			}
 		}
-		
-		else if(id == 6) {
+
+		else if (id == 6) {
 			List<Funcionario> allFuncionario = service.ListarFuncionarioEspecialidade(term);
 			for (Funcionario funcionario : allFuncionario) {
 				LabelValue lv = new LabelValue();
@@ -212,63 +217,104 @@ public class FuncionarioController {
 				suggeestions.add(lv);
 			}
 		}
-		
+
 		return suggeestions;
 	}
-	
+
 	@PostMapping("/editar")
-	public String editar(@Valid Funcionario funcionario, RedirectAttributes attr) {
+	public String editar(@Valid Funcionario funcionario, RedirectAttributes attr, @AuthenticationPrincipal User user) {
+
+		Funcionario f = service.buscarporId(funcionario.getId());
 		
-		attr.addFlashAttribute("success","Funcionario(a) alterado(a) com sucesso");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		if (!funcionario.getData_inicio().isEqual(f.getData_inicio())) {
+			// se a data de inicio editada for diferente da do banco, gera uma linha na
+			// tabela de log
+			Login login = lservice.BuscarPorCPF(user.getUsername());
+			Log l = new Log();
+			l.setData(LocalDate.now());
+			l.setFuncionario_id(login.getFuncionario_id());
+			l.setHora(LocalTime.now());
+			
+			l.setDescricao("EDIÇÃO DA DATA DE CONTRATAÇÃO DO FUNCIONARIO " + f.getNome() + " DE "
+					+ f.getData_inicio().format(formatter) + " PARA " + funcionario.getData_inicio().format(formatter));
+			logservice.salvar(l);
+		}
+		
+		if( f.getData_termino() != null && !funcionario.getData_termino().isEqual(f.getData_termino())) {
+			// se a data de termino editada for diferente da do banco, gera uma linha na
+			// tabela de log
+			Login login = lservice.BuscarPorCPF(user.getUsername());
+			Log l = new Log();
+			l.setData(LocalDate.now());
+			l.setFuncionario_id(login.getFuncionario_id());
+			l.setHora(LocalTime.now());
+			
+			l.setDescricao("EDIÇÃO DA DATA DE TERMINO DO FUNCIONARIO " + f.getNome() + " DE "
+					+ f.getData_termino().format(formatter) + " PARA " + funcionario.getData_termino().format(formatter));
+			logservice.salvar(l);
+		}
+			
+		attr.addFlashAttribute("success", "Funcionario(a) alterado(a) com sucesso");
 		service.salvar(funcionario);
 		return "redirect:/funcionario/listar";
 	}
-	
+
 	@GetMapping("/excluir/{id}")
-	public String excluir(@PathVariable("id") Long id, ModelMap model) {
-		model.addAttribute("success", "Funcionario(a) excluído(a) com sucesso");
+	public String excluir(@PathVariable("id") Long id, ModelMap model, @AuthenticationPrincipal User user) {
+		Funcionario f = service.buscarporId(id);
+		Login login = lservice.BuscarPorCPF(user.getUsername());
+		Log l = new Log();
+		l.setData(LocalDate.now());
+		l.setFuncionario_id(login.getFuncionario_id());
+		l.setHora(LocalTime.now());
+		l.setDescricao("EXCLUSÃO DO FUNCIONARIO " + f.getNome());
+		logservice.salvar(l);
 		service.excluir(id);
-		
+		model.addAttribute("success", "Funcionario(a) excluído(a) com sucesso");
 		return "redirect:/funcionario/listar";
 	}
-	
+
 	@PostMapping("/trocarSenha")
-	public String editarSenha(@RequestParam("senha1") String s1, @RequestParam("senha2") String s2, 
-			@RequestParam("senha3") String s3, @RequestParam("funcionario_id") Long funcionario_id,@AuthenticationPrincipal User user, RedirectAttributes attr) {
-		
-		
-		if(!s1.equals(s2)) {
+	public String editarSenha(@RequestParam("senha1") String s1, @RequestParam("senha2") String s2,
+			@RequestParam("senha3") String s3, @RequestParam("funcionario_id") Long funcionario_id,
+			@AuthenticationPrincipal User user, RedirectAttributes attr) {
+
+		if (!s1.equals(s2)) {
 			attr.addFlashAttribute("fail", "Senhas não conferem, tente novamente");
 			return "redirect:/funcionario/editar/" + funcionario_id;
 		}
-		
+
 		Login l = lservice.BuscarPorCPF(user.getUsername());
-		
-		if(!LoginService.isSenhaCorreta(s3, l.getSenha())) {
+
+		if (!LoginService.isSenhaCorreta(s3, l.getSenha())) {
 			attr.addFlashAttribute("fail", "Senha atual não confere, tente novamente");
-			
+
 			return "redirect:/funcionario/editar/" + funcionario_id;
 		}
-		
+
 		lservice.alterarSenha(l, s1);
 		attr.addFlashAttribute("success", "Senha alterado com sucesso!");
 		return "redirect:/funcionario/editar/" + funcionario_id;
 	}
-	
+
 	@GetMapping("/convenio/{id}/{funcId}")
-	public @ResponseBody List<TConvenio> listTipoConvenio(@PathVariable("id")Long id, @PathVariable("funcId") Long funcId) {
+	public @ResponseBody List<TConvenio> listTipoConvenio(@PathVariable("id") Long id,
+			@PathVariable("funcId") Long funcId) {
 		return tcService.BuscarTConvenioFunc(id, funcId);
 	}
 
 	@GetMapping("/allconvenios/{id}/{funcId}")
-	public @ResponseBody List<TConvenio> listAllTipoConvenio(@PathVariable("id") Long id, @PathVariable("funcId") Long funcId) {
+	public @ResponseBody List<TConvenio> listAllTipoConvenio(@PathVariable("id") Long id,
+			@PathVariable("funcId") Long funcId) {
 		return tcService.ListaComboBoxFunc(id, funcId);
 	}
-	
+
 	@PostMapping("/salvarTConv/{funcId}")
-	public String salvarTConv(Funcionario func, @PathVariable("funcId") Long funcId, @RequestParam("tconvenio") List<TConvenio> tconvenio) {
+	public String salvarTConv(Funcionario func, @PathVariable("funcId") Long funcId,
+			@RequestParam("tconvenio") List<TConvenio> tconvenio) {
 		List<FuncTConv> functconv = new ArrayList<FuncTConv>();
-		for(TConvenio tconvenios : tconvenio) {
+		for (TConvenio tconvenios : tconvenio) {
 			FuncTConv ftc = new FuncTConv();
 			ftc.setFuncionario(func);
 			ftc.setTconvenio(tconvenios);
@@ -277,14 +323,15 @@ public class FuncionarioController {
 		ftcService.salvarList(functconv);
 		return "redirect:/funcionario/editar/" + funcId;
 	}
-	
+
 	@PostMapping("exlcuirTConv/{funcId}")
-	public String excluirTConvenio(Funcionario func, @PathVariable("funcId") Long funcId, @RequestParam("tconvenio") List<TConvenio> tconvenio) {
-		for(TConvenio tconvenios : tconvenio) {
+	public String excluirTConvenio(Funcionario func, @PathVariable("funcId") Long funcId,
+			@RequestParam("tconvenio") List<TConvenio> tconvenio) {
+		for (TConvenio tconvenios : tconvenio) {
 			Long id = tconvenios.getId();
 			ftcService.deleteTConvFunc(id, funcId);
 		}
 		return "redirect:/funcionario/editar/" + funcId;
 	}
-	
+
 }
