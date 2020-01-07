48195c37c7027cab2c967f6f6a1230196300777e
Nadia Araujo <nadiaaraujo1@egmail.com>
2020-01-07 12:09:43 -0300

    Style2

--- src/main/resources/static/js/alerta.js
+++ src/main/resources/static/js/alerta.js
@@ -1,8 +1,1 @@
-function selector() {
-  $(".alert").stop().fadeTo(1, 1).removeClass('hidden');
-  window.setTimeout(function() {
-    $(".alert").fadeTo(500, 0).slideUp(500, function(){
-        $(".alert").addClass('hidden');
-    });
-  }, 1000); 
-}
+$().alert('close')
