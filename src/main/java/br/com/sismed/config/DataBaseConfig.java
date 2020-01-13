7e04c41d7516effff0cc232e02c297b435064f08
João Vitor <joaooviitorr@hotmail.com>
2020-01-13 20:51:13 -0300

    padronização de links

--- src/main/java/br/com/sismed/config/DataBaseConfig.java
+++ src/main/java/br/com/sismed/config/DataBaseConfig.java
@@ -1,42 +1,80 @@
 package br.com.sismed.config;
 
-import java.sql.SQLException;
+
 
 import javax.sql.DataSource;
 
-import org.hibernate.JDBCException;
-import org.hibernate.exception.GenericJDBCException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.CannotGetJdbcConnectionException;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.support.MetaDataAccessException;
+
+
+import com.zaxxer.hikari.HikariDataSource;
 
 
 @EnableJpaRepositories
 @Configuration
 public class DataBaseConfig {
-
-	@Bean
-    public DataSource data() {
-		 DriverManagerDataSource bd = new DriverManagerDataSource();
-		 bd.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		try {
-       
-       System.out.println("Entrei no try");
-        bd.setUrl("jdbc:mysql://wjoodes04v:3306/tresta?useTimezone=true&serverTimezone=America/Sao_Paulo");
-        bd.setUsername("tresta");
-        bd.setPassword("Mass55");
-		}catch(CannotGetJdbcConnectionException g) {
-			 System.out.println("Entrei no catch");
-	        bd.setUrl("jdbc:mysql://localhost:3306/tresta?useTimezone=true&serverTimezone=America/Sao_Paulo");
-	        bd.setUsername("root");
-	        bd.setPassword("172010joao");
-		}
-        
-        return bd;
-        
-        
-    }
+	 Logger logger = LoggerFactory.getLogger(DataBaseConfig.class);
+	 
+	    @Bean
+	    public DataSource dataSource() {
+	        DataSource dataSource;
+	        try {
+	            dataSource = dataSource1();
+	            dataSource.getConnection().isValid(500);
+	        } catch (Exception e) {
+	            logger.warn(">>> DataSource 1 falhou, conectado no 2. <<<", e.getMessage());
+	            dataSource = dataSource3();
+	            
+	        }
+	        return dataSource;
+	    }
+	 
+	    private DataSource dataSource1() {
+	        HikariDataSource ds = new HikariDataSource();
+	        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	        ds.setJdbcUrl("jdbc:mysql://wjoodes04v:3306/tresta?useTimezone=true&serverTimezone=America/Sao_Paulo");
+	        ds.setUsername("tresta");
+	        ds.setPassword("Mass55");
+	        return ds;
+	    }
+	 
+	    private DataSource dataSource2() {
+	        HikariDataSource ds = new HikariDataSource();
+	        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	        ds.setJdbcUrl("jdbc:mysql://wmysdes01v:3306/tresta?useTimezone=true&serverTimezone=America/Sao_Paulo");
+	        ds.setUsername("tresta");
+	        ds.setPassword("Mass55");
+	        return ds;
+	    }
+	    
+	    private DataSource dataSource3() {
+	        HikariDataSource ds = new HikariDataSource();
+	        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	        ds.setJdbcUrl("jdbc:mysql://localhost:3306/tresta?useTimezone=true&serverTimezone=America/Sao_Paulo");
+	        ds.setUsername("root");
+	        ds.setPassword("172010joao");
+	        return ds;
+	    }
+	    
+	    private DataSource dataSource4() {
+	        HikariDataSource ds = new HikariDataSource();
+	        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	        ds.setJdbcUrl("jdbc:mysql://localhost:3306/tresta?useTimezone=true&serverTimezone=America/Sao_Paulo");
+	        ds.setUsername("root");
+	        ds.setPassword("1234");
+	        return ds;
+	    }
+	    
+	    private DataSource dataSource5() {
+	        HikariDataSource ds = new HikariDataSource();
+	        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	        ds.setJdbcUrl("jdbc:mysql://localhost:3306/tresta?useTimezone=true&serverTimezone=America/Sao_Paulo");
+	        ds.setUsername("root");
+	      
+	        return ds;
+	    }
 }
