0bd9fd63d4d2a8dcb0ccf61214b1541503793bde
João Vitor <joaooviitorr@hotmail.com>
2020-02-13 14:29:22 -0300

    Atualização

---
+++ src/main/resources/static/js/relatorio/limparCampos.js
@@ -0,0 +1,16 @@
+function limparCampos(){
+	
+	$("#pacienteCheck").prop("checked", false);
+	$("#funcionarioCheck").prop("checked", false);
+	$("#convenioCheck").prop("checked", false);
+	$("#periodoCheck").prop("checked", false);
+	$("#paciente").val(null);
+	$("#funcionario").val(null);
+	$("#convenio").val(null);
+	$("#periodo").val(null);
+	
+	
+	
+	
+	
+}
