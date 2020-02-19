24141c59c4631f48405d7495c3a2d5d3702e9aed
João Vitor <joaooviitorr@hotmail.com>
2020-02-19 12:30:48 -0300

    Pagina de carregamento do backup

--- src/main/java/br/com/sismed/web/controller/BackupController.java
+++ src/main/java/br/com/sismed/web/controller/BackupController.java
@@ -1,13 +1,15 @@
 package br.com.sismed.web.controller;
 
-import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -55,36 +57,48 @@ public class BackupController {
 		ResultSet r = s.executeQuery("SHOW TABLES;");
 		List<String> tabelas = new ArrayList<String>();
 		while (r.next()) {
-			tabelas.add(r.getString("Tables_in_sismed"));
+			tabelas.add(r.getString("Tables_in_tresta"));
 		}
 		model.addAttribute("tabelas", tabelas);
 		return "backup/tabelas";
 	}
 
 	@PostMapping("/gerar")
-	public String gerarBackup(@RequestParam("tabelas") List<String> tabelas) {
+	public String gerarBackup(@RequestParam("tabelas") List<String> tabelas, @AuthenticationPrincipal User user) {
+		
+		Login usuario = lservice.BuscarPorCPF(user.getUsername());
 		String tables = "";
 		
+
 		for (String t : tabelas) {
 			tables += t + " ";
 		}
-		String dump = "mysqldump -u " + host + " -p" + password + " " + dataBase + " " + tables + " > " + LocalDate.now() + ".sql";
-				
-		String[] comando = {"cd d:\\xampp\\mysql\\bin", dump};
+
+		String caminho = "D:\\backup\\";
+
+		String dump = "mysqldump -u " + host + " -p" + password + " " + dataBase + " " + tables + " > " + caminho
+				+ LocalDate.now() + ".sql";
+
+		String[] comando = { "cd d:\\xampp\\mysql\\bin", dump };
+		
 		try {
+			
 			ProcessBuilder builder = new ProcessBuilder("cmd", "/c", String.join("& ", comando));
 			builder.redirectErrorStream(true);
 			builder.start();
+			FileWriter arq = new FileWriter(caminho + "informações.txt");
+			PrintWriter gravarArquivo = new PrintWriter(arq);
+			gravarArquivo.print("Backup realizado por " + usuario.getFuncionario_id().getNome() + " no dia " + dataHoraAtual());
 			
+			arq.close();
 			
-			
-			
-		}catch(Exception a) {
+		} catch (Exception a) {
 			a.printStackTrace();
 		}
-		
-		return "redirect:/backup";
+
+		return "backup/carregamento";
 	}
+	
 
 	@ModelAttribute("usuarioLogado")
 	public String usuarioLogado(@AuthenticationPrincipal User user, ModelMap model) {
@@ -104,5 +118,15 @@ public class BackupController {
 		}
 
 		return retorno;
+	}
+
+	String dataHoraAtual() {
+		DateTimeFormatter dataFormatter =  DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter horaFormatter =  DateTimeFormatter.ofPattern("HH:mm:ss");
+		LocalDate data = LocalDate.now();
+		LocalTime hora = LocalTime.now();
+		String dataFormatada = dataFormatter.format(data);
+		String horaFormatada = horaFormatter.format(hora);
+		return dataFormatada + " As " + horaFormatada;
 	}
 }
---
+++ src/main/resources/static/js/backup/mostrarTexto.js
@@ -0,0 +1,10 @@
+ $(document).ready(function() {
+	
+	 window.setTimeout('mostrarTexto()', 5000);
+ });
+
+ function mostrarTexto(){
+	 $('#gerandoBackup').css("display", "none");
+	 $('#arquivoGerado').css("display", "block");
+ }
+    
